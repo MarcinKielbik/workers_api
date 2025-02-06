605cddaba73d1e69d517ced43b3bb34e888333d3
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-02-06 21:03:32 +0100

    Create Worker model and serializer

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Worker(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    company = models.CharField(max_length=255)
+    department = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(unique=True)
+    citizenship = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.company}"
