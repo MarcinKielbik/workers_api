2f6096b2acf703c7c314941090a775c5c7b93ef0
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-02-13 21:07:37 +0100

    Added 'salary' and 'employment_type' fields to Worker model

--- employees/models.py
+++ employees/models.py
@@ -9,6 +9,12 @@ class Worker(models.Model):
     phone = models.CharField(max_length=20, unique=True)
     email = models.EmailField(unique=True)
     citizenship = models.CharField(max_length=50)
+    salary = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    employment_type = models.CharField(max_length=50, choices=[  # Forma zatrudnienia
+        ('full_time', 'Full-time'),
+        ('part_time', 'Part-time'),
+        ('contract', 'Contract'),
+    ], default='full_time')
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} - {self.company}"
